1e4d36e8a6f0b9fa5e140e38127e40a8dd8569ff
TonyCross23 <tonychamp40@gmail.com>
2025-05-04 15:29:45 +0630

    server

--- server/config/default.ts
+++ server/config/default.ts
@@ -1,6 +1,10 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
 export default {
     port: 4000,
-    dbUri: "mongodb://localhost:27017/short",
+    dbUri: process.env.dbUri,
     host: 'localhost',
     corsOrigin: "http://localhost:3000",
 }
